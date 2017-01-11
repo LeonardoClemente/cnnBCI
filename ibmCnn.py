768acae4860394e28fa739a5f0e7e31a3626b54f
Cesar Leonardo Clemente López <clemclem1991@gmail.com>
2017-01-10 22:53:21 -0600

    Update ibmCnn.py

--- ibmCnn.py
+++ ibmCnn.py
@@ -48,14 +48,6 @@ sess=tf.InteractiveSession()
 
 
 
-k = io.loadmat('decimatedVectors.mat')
-kk = io.loadmat('epochTargets.mat')
-
-data = k['decimatedVectors'] # Data turned
-targets = kk['data_key_D'] # Data is just a 1 dimensional vector. Due to scipys format data is called using double bracket notation ex. ''targets[0][1]''
-			
-
-
 def weight_variable(shape, nom):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial, name = nom )
